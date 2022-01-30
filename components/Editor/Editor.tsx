954ee87013140c38575a4eceba2ce0c0c3495831
simpsonphile <com:simpsonphile@gmail.com>
2022-01-30 16:27:05 +0100

    Clean up editor component

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,9 +1,8 @@
-import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror';
+import CodeMirror from '@uiw/react-codemirror';
 import { oneDark } from '@codemirror/theme-one-dark';
 import { javascript } from '@codemirror/lang-javascript';
 
 import styles from './index.module.scss';
-import { MutableRefObject, Ref, RefAttributes, useEffect, useRef } from 'react';
 
 type EditorProps = {
   value?: string;
@@ -13,21 +12,9 @@ type EditorProps = {
 export default function Editor(props: EditorProps): React.ReactElement {
   const { value, setValue } = props;
 
-  const cmRef = useRef<any>();
-
-  console.log(cmRef);
-
-  useEffect(() => {
-    if (cmRef.current.state) {
-      console.log(cmRef.current.state);
-      cmRef.current.state.config.lineWrapping = true;
-    }
-  }, [cmRef]);
-
   return (
     <div className={styles.editor}>
       <CodeMirror
-        ref={cmRef}
         className={styles['editor__textarea']}
         value={value}
         extensions={[javascript()]}
