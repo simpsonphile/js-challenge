19973720a7a9887869848dc8c8fad1b03ed58739
simpsonphile <com:simpsonphile@gmail.com>
2022-02-02 14:46:18 +0100

    Fix fontScales

--- themes/fontScales.tsx
+++ themes/fontScales.tsx
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 const fontScales = {
   xs: ['1rem', '1.6rem'],
   s: ['1.2rem', '1.8rem'],
-  base: ['1.6rem', 'rem'],
+  base: ['1.6rem', '2.4rem'],
   lg: ['2rem', '3rem'],
   xl: ['2.4rem', '3.6rem'],
   xl2: ['3.2rem', '4.8rem'],
