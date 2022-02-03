97a319fef6a4880731e1deef7423a4c49768bb22
simpsonphile <com:simpsonphile@gmail.com>
2022-02-03 13:54:44 +0100

    Add padding to header

--- components/Header/Header.styles.ts
+++ components/Header/Header.styles.ts
@@ -4,6 +4,8 @@ const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
 
+  padding: ${({ theme }) => theme.spaces[3]};
+
   background-color: ${({ theme }) => theme.color.bg.shade(5)};
 `;
 
