471b02c4f3f0a492c5b7975d2fbaf204e6b602b4
simpsonphile <com:simpsonphile@gmail.com>
2022-02-03 20:49:19 +0100

    Make modal exercise second button secondary

--- components/ModalExerciseCompleted/ModalExerciseCompleted.tsx
+++ components/ModalExerciseCompleted/ModalExerciseCompleted.tsx
@@ -36,6 +36,7 @@ export default function ModalExerciseCompleted(
           </Button>
 
           <Button
+            genre="secondary"
             onClick={() => {
               goToRandomExercise?.();
               onClose();
