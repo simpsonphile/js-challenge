cc86a591ae878dae9044b93678496c40496dc97f
simpsonphile <com:simpsonphile@gmail.com>
2022-02-05 19:01:09 +0100

    Fix breakopints values

--- themes/config.ts
+++ themes/config.ts
@@ -35,11 +35,11 @@ const radiss = ['0rem', '0.4rem', '0.8rem'];
 // Breakpoints
 /***********************************************************/
 const breakpoints = {
-  sm: 640,
-  md: 768,
-  lg: 1024,
-  xl: 1280,
-  '2xl': 1536,
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
 };
 
 // Colors
--- themes/mixins/mediaQueries.ts
+++ themes/mixins/mediaQueries.ts
@@ -3,6 +3,6 @@ import { breakpoints } from '../config';
 // todo add media Queries like hover etc
 
 const mediaMinWidth = (breakpoint: keyof typeof breakpoints) =>
-  `@media (min-width: ${breakpoints[breakpoint] + 'px'})`;
+  `@media (min-width: ${breakpoints[breakpoint]}`;
 
 export default mediaMinWidth;
