280bd73f7bf30a5a9877a5d8be4423efbcb322d4
simpsonphile <com:simpsonphile@gmail.com>
2022-02-06 16:17:58 +0100

    Improve mediaQueries

--- themes/mixins/mediaQueries.ts
+++ themes/mixins/mediaQueries.ts
@@ -1,11 +1,19 @@
-import { css, DefaultTheme, ThemedStyledFunction } from 'styled-components';
+import {
+  css,
+  DefaultTheme,
+  FlattenInterpolation,
+  FlattenSimpleInterpolation,
+  ThemeProps,
+} from 'styled-components';
 import { breakpoints } from '../config';
 
 type BreakpointKeys = keyof typeof breakpoints;
 
 type MediaMinWidth = Record<
   BreakpointKeys,
-  ThemedStyledFunction<'div', DefaultTheme, object, never>
+  (
+    cssTemplate: FlattenInterpolation<ThemeProps<DefaultTheme>>
+  ) => FlattenSimpleInterpolation
 >;
 
 const breakpointKeys = Object.keys(breakpoints) as BreakpointKeys[];
@@ -14,9 +22,11 @@ const mediaMinWidth = breakpointKeys.reduce(
   (acc: MediaMinWidth, cur: BreakpointKeys) => {
     return {
       ...acc,
-      [cur]: (...args: TemplateStringsArray) => css`
+      [cur]: (
+        cssTemplate: FlattenInterpolation<ThemeProps<DefaultTheme>>
+      ) => css`
         @media (min-width: ${breakpoints[cur]}) {
-          ${css(args)}
+          ${cssTemplate}
         }
       `,
     };
