8e6ac2aac97a2b46673fb4d7776c1e3776093cfd
simpsonphile <com:simpsonphile@gmail.com>
2022-02-09 13:06:28 +0100

    Theme switcher changes

--- components/ThemeSwitcher/ThemeSwitcher.tsx
+++ components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,17 +1,23 @@
 import { useContext } from 'react';
+import { WiDaySunny, WiNightClear } from 'react-icons/wi';
 
-import Button from 'components/Button';
 import ThemeContext from 'themes/ThemeContext';
+import { useTheme } from 'styled-components';
+import Button from 'components/Button';
 
 export default function ThemeSwitcher(): React.ReactElement {
   const themeContext = useContext(ThemeContext);
-
+  const { color } = useTheme();
   const { themeName, setTheme } = themeContext;
   const nextTheme = themeName === 'dark' ? 'light' : 'dark';
 
+  const Icon = themeName === 'dark' ? WiDaySunny : WiNightClear;
+
   return (
     <div>
-      <Button onClick={() => setTheme?.(nextTheme)}>{nextTheme}</Button>
+      <Button genre="plain" onClick={() => setTheme?.(nextTheme)}>
+        <Icon size="3.2rem" strokeWidth="1" color={color.main} />
+      </Button>
     </div>
   );
 }
