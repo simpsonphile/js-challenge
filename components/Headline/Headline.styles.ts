9ded5d1625a002a779424ed69b93555c32cf4225
simpsonphile <com:simpsonphile@gmail.com>
2022-02-10 13:44:35 +0100

    Add color type to headline

--- components/Headline/Headline.styles.ts
+++ components/Headline/Headline.styles.ts
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import { colorMixin, ColorProps } from 'themes/mixins/color';
 import getFontScale, { FontScaleProps } from 'themes/mixins/getFontScale';
 
-export type StyledHeadlineProps = FontScaleProps & ColorProps;
+export type StyledHeadlineProps = FontScaleProps & {
+  color: ColorProps['color'];
+};
 
 const StyledHeadline = styled.h1<StyledHeadlineProps>`
   ${getFontScale};
