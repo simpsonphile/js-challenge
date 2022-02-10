3baee097c2b2b6f9c6a15f789144f0c85a95c856
simpsonphile <com:simpsonphile@gmail.com>
2022-02-10 14:28:57 +0100

    Fix results count

--- components/Results/Results.tsx
+++ components/Results/Results.tsx
@@ -17,9 +17,9 @@ export default function Results(props: ResultsProps): React.ReactElement {
 
   const resultNodes = results.map(
     ({ result, failMessage, successMessage }, index) => {
-      let text = failMessage || `test ${index} failed`;
+      let text = failMessage || `test ${index + 1} failed`;
       if (result) {
-        text = successMessage || `test ${index} passed`;
+        text = successMessage || `test ${index + 1} passed`;
       }
 
       return (
