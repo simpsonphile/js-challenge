9a2dcf6cedf18e4c48ffa5f65bf26a04a5f5b138
simpsonphile <com:simpsonphile@gmail.com>
2022-02-11 13:03:14 +0100

    Add fullScreen component

---
+++ components/FullScreen/FullScreen.tsx
@@ -0,0 +1,26 @@
+import { MutableRefObject, useRef } from 'react';
+import { useFullscreen } from 'react-use';
+
+import { StyledFullScreen } from './FullScreen.style';
+
+type FullScreenProps = {
+  children: React.ReactElement;
+  toggle?: (state: boolean) => void;
+  state: boolean;
+};
+
+export default function FullScreen(props: FullScreenProps): React.ReactElement {
+  const { children, toggle, state } = props;
+
+  const isBrowser = typeof window === 'object';
+
+  console.log(state);
+
+  const ref = useRef<HTMLDivElement>() as MutableRefObject<HTMLDivElement>;
+
+  useFullscreen(ref, isBrowser && state, {
+    onClose: () => toggle?.(false),
+  });
+
+  return <StyledFullScreen ref={ref}>{children}</StyledFullScreen>;
+}
