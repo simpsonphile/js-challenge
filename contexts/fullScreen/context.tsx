f395d977e53f43100040a59045637996b9b4828b
simpsonphile <com:simpsonphile@gmail.com>
2022-02-14 15:30:20 +0100

    fix

--- contexts/fullScreen/context.tsx
+++ contexts/fullScreen/context.tsx
@@ -1,18 +1,5 @@
 import React from 'react';
 
-export type FullScreen = {
-  id: string;
-  fullSlug: string;
-  slug: string;
-  cat?: string | null;
-  title?: string;
-  hints?: string;
-  solution?: string;
-  code?: string;
-  tests?: string;
-  isPassed?: boolean;
-};
-
 export type FullScreenContextType = {
   isFullScreen: boolean;
   toggleFullScreen?: () => void;
