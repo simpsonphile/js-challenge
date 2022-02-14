e2b0622a3d9bb2f85ccb42ed682892d726690911
simpsonphile <com:simpsonphile@gmail.com>
2022-02-14 15:28:24 +0100

    add padding to full screen

--- components/FullScreen/FullScreen.styles.ts
+++ components/FullScreen/FullScreen.styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const StyledFullScreen = styled.div`
   :fullscreen {
+    padding: ${({ theme }) => theme.spacing.lg};
+
     background-color: ${({ theme }) => theme.color.bg};
   }
 `;
