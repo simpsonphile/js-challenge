b9d5985f35d8848b75424a26512837732448f7b4
simpsonphile <com:simpsonphile@gmail.com>
2022-02-22 20:14:57 +0100

    Add shortcust to modal info

--- components/ModalInfo/ModalInfo.tsx
+++ components/ModalInfo/ModalInfo.tsx
@@ -1,35 +1,65 @@
-import Box from 'components/Box';
+import React from 'react';
+
+import Box, { BoxDefault } from 'components/Box';
+import Headline from 'components/Headline';
+import Inline from 'components/Inline';
+import { Kbd } from 'components/Kbd';
 import Modal from 'components/Modal';
-import Text from 'components/Text';
 
 type ModalInfoProps = {
   onClose: () => void;
 };
 
+const items = [
+  {
+    title: 'focus search input',
+    keys: ['⌘', 'K'],
+  },
+  {
+    title: 'test code',
+    keys: ['⌘', '⌥', 't'],
+  },
+  {
+    title: 'change theme',
+    keys: ['⌘', '⌥', 's'],
+  },
+  {
+    title: 'full screen',
+    keys: ['⌘', '⌥', 'E'],
+  },
+];
+
 export default function ModalInfo(props: ModalInfoProps): React.ReactElement {
   const { onClose } = props;
 
+  const elements = React.useMemo(
+    () =>
+      items.map(({ title, keys }) => (
+        <div key={title}>
+          <Box $mb="s">
+            <Headline $fs="base">{title}:</Headline>
+          </Box>
+
+          <Inline $g="xs">
+            {keys.map((key) => (
+              <Kbd key={key}>{key}</Kbd>
+            ))}
+          </Inline>
+        </div>
+      )),
+    []
+  );
+
   return (
     <Modal onClose={onClose}>
-      <Box $bgColor="bg" $p="base">
-        <Text>
-          <h2>Shortcuts</h2>
-          <ul>
-            <li>
-              focus search input: <strong>⌘ + K</strong>
-            </li>
-            <li>
-              test code: <strong>⌘ + ⌥ + t</strong>
-            </li>
-            <li>
-              change theme: <strong>⌘ + ⌥ + s</strong>
-            </li>
-            <li>
-              full screen: <strong>⌘ + ⌥ + E</strong>
-            </li>
-          </ul>
-        </Text>
-      </Box>
+      <BoxDefault>
+        <Inline $g="lg" $direction="column">
+          <Headline>Shortcuts</Headline>
+          <Inline $g="base" $direction="column">
+            {elements}
+          </Inline>
+        </Inline>
+      </BoxDefault>
     </Modal>
   );
 }
