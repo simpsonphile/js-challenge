2c4517504adbc883f66619693c642f11f9040711
simpsonphile <com:simpsonphile@gmail.com>
2022-02-24 09:49:07 +0100

    Add edit

--- views/ExerciseView/components/Tabs/Tabs.tsx
+++ views/ExerciseView/components/Tabs/Tabs.tsx
@@ -1,18 +1,25 @@
+import { useContext } from 'react';
+
 import ExerciseEditor from 'components/ExerciseEditor';
 import BaseTabs, { TabsProps as BaseTabsProps } from 'components/Tabs';
+import { ExerciseContext } from 'contexts/exercises';
+import routes from 'lib/routes';
 
 type TabsProps = {
   id: string;
-  code?: string;
-  tests?: string;
-  hints?: string;
   onSuccess: () => void;
 };
 
 export default function Tabs(props: TabsProps): React.ReactElement {
-  const { id, hints, onSuccess } = props;
+  const { id, onSuccess } = props;
 
   const tabItems: BaseTabsProps['items'] = {};
+
+  const { getExerciseById } = useContext(ExerciseContext);
+  const exercise = getExerciseById(id);
+  const hints = exercise?.hints;
+  const slug = exercise?.fullSlug;
+  const url = slug ? routes.editExerciseOnGithub(slug) : undefined;
 
   if (id) {
     tabItems.code = {
@@ -29,6 +36,13 @@ export default function Tabs(props: TabsProps): React.ReactElement {
   if (hints) {
     tabItems.hints = {
       content: <div>{hints}</div>,
+    };
+  }
+
+  if (url) {
+    tabItems.edit = {
+      asLink: true,
+      href: url,
     };
   }
 
