baf8afcd54c5bc32802d7abd98ac5412289ae0c7
simpsonphile <com:simpsonphile@gmail.com>
2022-03-16 12:14:45 +0100

    Add chai, add test for editor

---
+++ cypress/integration/editor.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+
+describe('Editor', () => {
+  it('it saves editor text', () => {
+    cy.viewport(1200, 800);
+
+    cy.visit('/');
+
+    let text: string | string[] | number = '';
+
+    cy.findByTestId('code-editor').within((editor) => {
+      const textarea = cy.get('textarea');
+
+      textarea.type('heheeheh', { force: true });
+
+      text = editor.text();
+    });
+
+    let hasFoundExpandedAccord = false;
+
+    cy.findAllByTestId('accord').each((accord) => {
+      if (accord.attr('aria-expanded') === 'true' || hasFoundExpandedAccord)
+        return false;
+
+      hasFoundExpandedAccord = true;
+
+      cy.wrap(accord).click().findByTestId('exercise-link').first().click();
+    });
+
+    cy.visit('/');
+
+    cy.findByTestId('code-editor').within((editor) => {
+      expect(editor.text()).to.equal(text);
+    });
+  });
+});
+
+export {};
