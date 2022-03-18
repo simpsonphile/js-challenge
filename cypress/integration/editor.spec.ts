a6e2e85be7ac841c372a46cd169143cabbfe9cfa
simpsonphile <com:simpsonphile@gmail.com>
2022-03-18 16:08:04 +0100

    Add editor spec: correct answer

--- cypress/integration/editor.spec.ts
+++ cypress/integration/editor.spec.ts
@@ -1,6 +1,9 @@
 import { faker } from '@faker-js/faker';
 import { expect } from 'chai';
 
+import { Exercise } from 'lib/getExercises';
+import routes from 'lib/routes';
+
 describe('Editor', () => {
   it('saves editor text that stays even after route change', () => {
     cy.viewport(1200, 800);
@@ -34,6 +37,49 @@ describe('Editor', () => {
 
     cy.findByTestId('code-editor').within((editor) => {
       expect(editor.text()).to.equal(text);
+    });
+  });
+
+  it('accept correct answer, make it complete, show modal', async () => {
+    cy.viewport(1200, 800);
+
+    cy.visit('/');
+
+    cy.task<Exercise[]>('getExercises').then((exercises: Exercise[]) => {
+      const exercise = exercises[1];
+
+      const { answer, fullSlug } = exercise;
+
+      const exerciseUrl = routes.exercise(fullSlug);
+
+      cy.visit(exerciseUrl);
+
+      cy.findByTestId('code-editor').within((editor) => {
+        const textarea = cy.get('textarea');
+
+        textarea.type('{command}a', { force: true });
+        textarea.type('{backspace}{backspace}{backspace}', {
+          force: true,
+        });
+        textarea.type(answer || '', {
+          force: true,
+          parseSpecialCharSequences: false,
+        });
+      });
+
+      cy.findByRole('button', {
+        name: /test/i,
+      }).click();
+
+      cy.findByRole('dialog')
+        .findByRole('button', {
+          name: /Go to next exercise/i,
+        })
+        .click();
+
+      cy.visit(exerciseUrl);
+
+      cy.findByTestId('exercise-title').contains('✅');
     });
   });
 });
