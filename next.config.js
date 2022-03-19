4d4c3243ba2b17b4c0c0d27cd16d7181471f758f
simpsonphile <com:simpsonphile@gmail.com>
2022-03-19 12:24:30 +0100

    Deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   // reactStrictMode: true,
+  assetPrefix: prod ? 'https://simpsonphile.github.io/js-challenge/' : '',
   compiler: {
     styledComponents: true,
   },
