d4e3ba383c8e5401e771995a827e064a005557c3
simpsonphile <com:simpsonphile@gmail.com>
2022-04-24 13:04:22 +0200

    Add button test disabled no emit

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -41,3 +41,24 @@ test('emit onClick when clicked', () => {
 
   expect(buttonElement).toBeTruthy();
 });
+
+test('do not emit onClick when clicked and disabled', () => {
+  const handleOnClick = jest.fn();
+  const component = (
+    <Button disabled onClick={handleOnClick}>
+      hello
+    </Button>
+  );
+
+  render(component);
+
+  const buttonElement = document.querySelector('button');
+
+  if (buttonElement) {
+    fireEvent.click(buttonElement);
+
+    expect(handleOnClick).toBeCalledTimes(0);
+  }
+
+  expect(buttonElement).toBeTruthy();
+});
