f99adde14f8b2b4c42327e07900dfdb152530236
simpsonphile <com:simpsonphile@gmail.com>
2022-04-24 13:34:14 +0200

    Add tests for single accord

---
+++ components/Accord/Accord.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import { render } from 'tests/test-utils';
+
+import Accord from './Accord';
+
+test('by default renders with content hidden and aria-hidden true', () => {
+  const component = <Accord title="accord" content="hello" />;
+
+  render(component);
+
+  const accordElement = screen.getByTestId('accord');
+  const accordContent = screen.getByTestId('accord-content');
+
+  expect(accordElement).toBeInTheDocument();
+
+  expect(accordContent).toHaveAttribute('aria-hidden', 'true');
+  expect(accordContent).not.toBeVisible();
+});
+
+test('when isActive set to true content is visible and aria-hidden set to false', () => {
+  const component = <Accord title="accord" content="hello" isActive />;
+
+  render(component);
+
+  const accordElement = screen.getByTestId('accord');
+  const accordContent = screen.getByTestId('accord-content');
+
+  expect(accordElement).toBeInTheDocument();
+
+  expect(accordContent).toHaveAttribute('aria-hidden', 'false');
+  expect(accordContent).toBeVisible();
+});
