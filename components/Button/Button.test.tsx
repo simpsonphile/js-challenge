1fb40e766ffbd52e7f467818e958604388119712
simpsonphile <com:simpsonphile@gmail.com>
2022-04-24 13:00:20 +0200

    Test if button emits onClick correctly

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
+import { fireEvent } from '@testing-library/react';
+
 import { render } from 'tests/test-utils';
 
 import Button from './Button';
 
-test('Button renders string children if set', () => {
-  const component = <Button>hello</Button>;
+test('renders string children if set', () => {
+  const text = 'hello';
+  const component = <Button>{text}</Button>;
 
   const { getByText } = render(component);
 
-  expect(getByText('hello')).toBeTruthy();
+  expect(getByText(text)).toBeTruthy();
 });
 
-test('Button has set type="button" by default', () => {
+test('has set type="button" by default', () => {
   const component = <Button>hello</Button>;
 
   render(component);
@@ -20,4 +23,21 @@ test('Button has set type="button" by default', () => {
   const buttonElement = document.querySelector('button');
 
   expect(buttonElement).toHaveAttribute('type', 'button');
+});
+
+test('emit onClick when clicked', () => {
+  const handleOnClick = jest.fn();
+  const component = <Button onClick={handleOnClick}>hello</Button>;
+
+  render(component);
+
+  const buttonElement = document.querySelector('button');
+
+  if (buttonElement) {
+    fireEvent.click(buttonElement);
+
+    expect(handleOnClick).toBeCalledTimes(1);
+  }
+
+  expect(buttonElement).toBeTruthy();
 });
